b3ede49a4e505b2bf61976becd7e3904f6a8bc7f
zhitiao <2052118560@qq.com>
2026-06-13 17:17:37 +0800

    能达到breakfast manet不报错

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_manet.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_manet-user \
-    lineage_manet-userdebug \
-    lineage_manet-eng
+#COMMON_LUNCH_CHOICES := \
+#    lineage_manet-user \
+#    lineage_manet-userdebug \
+#    lineage_manet-eng
